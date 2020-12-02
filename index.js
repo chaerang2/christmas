7c3ac116df1f0150c6753747f47f632897ff96bc
chaerang2 <70747507+chaerang2@users.noreply.github.com>
2020-12-02 23:30:22 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -4,31 +4,29 @@ $(function(){
         $('#schedule').removeClass('show')
         $('#song_form').removeClass('show')
         $('#yullmoo').removeClass('show')
-        return false;
+        $('#contents').removeClass('show')
       });
     $('.sche_btn').click(function(){
         $('#intro').css('display','none')
         $('#schedule').addClass('show')
         $('#song_form').removeClass('show')
         $('#yullmoo').removeClass('show')
-        return false;
+        $('#contents').removeClass('show')
       });
     $('.song_btn').click(function(){
         $('#intro').css('display','none')
         $('#schedule').removeClass('show')
         $('#song_form').addClass('show')
         $('#yullmoo').removeClass('show')
-        // 색
-        return false;
+        $('#contents').removeClass('show')
       });
       $('#yullmoo_btn').click(function(){
         $('#intro').css('display','none')
         $('#schedule').removeClass('show')
         $('#song_form').removeClass('show')
         $('#yullmoo').addClass('show')
-        // 색
-        return false;
+        $('#contents').addClass('show')
       });
     
   });
-  console.log('리액트로 짠걸 깃허브페이지를 쓰고싶어서 html,css로 짜다보니 스크롤 내려가는 것도 구현이 안되고(리액트는 됩니다 구질구질..) 이것저것 엉망입니다.. 부디.. 코드 뜯어보지 마시옵고... 복받으세요')
+  console.log('리액트로 짠걸 깃허브페이지를 쓰고싶어서 html,css로 짜다보니 이것저것 덕지덕지 엉망입니다.. 부디.. 코드 뜯어보지 마시옵고... 복받으세요')
